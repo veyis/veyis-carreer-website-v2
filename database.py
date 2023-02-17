eb82d621191d2cef975d4ad688fdfd89620ef878
vakgul <vakgul@gmail.com>
2023-02-17 22:02:32 +0000

    getjob page

--- database.py
+++ database.py
@@ -21,7 +21,7 @@ def load_jobs_from_db():
 
 def load_job_from_db(id):
   with engine.connect() as conn:
-    result = conn.execute(text('select * from jobs where id=:val '), val=id)
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = 1 "))
     rows = result.all()
     if len(rows) == 0:
       return None
