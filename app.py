1e1f18a4016914300b2363adb15568150df21a4c
vakgul <vakgul@gmail.com>
2023-02-17 21:45:07 +0000

    show_job added

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ def list_jobs():
 @app.route("/job/<id>")
 def show_job(id):
   job = load_job_from_db(id)
+
+  if not job:
+    return "Not Found", 404
+
   return render_template('jobpage.html', job=job)
 
 
