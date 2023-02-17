0ae0b22e753a27d0f565e076320e7cc601bbdade
vakgul <vakgul@gmail.com>
2023-02-17 21:36:00 +0000

    get job 2

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def list_jobs():
 @app.route("/job/<id>")
 def show_job(id):
   job = load_job_from_db(id)
-  return jsonify(job)
+  return render_template('jobpage.html', job=job)
 
 
 if __name__ == "__main__":
