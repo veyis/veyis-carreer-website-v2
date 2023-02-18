b76506ed8fa6773cf86a63254b4a6b11365f7ad8
vakgul <>
2023-02-18 05:10:32 +0000

    fix error

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def hello_jovian():
   jobs = load_jobs_from_db()
-  return render_template('home.html', jobs=jobs, company_name='Sorena LLC')
+  return render_template('home.html', jobs=jobs)
 
 
 @app.route("/api/jobs")
@@ -26,5 +26,21 @@ def show_job(id):
   return render_template('jobpage.html', job=job)
 
 
-if __name__ == "__main__":
+@app.route("/api/job/<id>")
+def show_job_json(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  return render_template('application_submitted.html',
+                         application=data,
+                         job=job)
+
+
+if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
